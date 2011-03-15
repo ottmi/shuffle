e07e6c1d237d308fc030ad86e96c9fd43a96000c
Michael Ott <michael.ott@csiro.au>
2011-03-15 11:24:50 +1100

    * Now printing syntax if called without parameters or -h * Minor cosmetical changes

--- src/shuffle.cpp
+++ src/shuffle.cpp
@@ -13,39 +13,41 @@ int verbose = 0;
 typedef struct opt_struct
 {
 	string inputAlignment;
-	string outputAlignment;
 	int dataType;
+	string outputAlignment;
 	int randomizations;
 	string summaryFile;
 	double minCompatibility;
 	double minPOC;
 	int maxMNIC;
 	double maxEntropy;
+	int help;
 } Options;
 
 void parseArguments(int argc, char** argv, Options *options)
 {
 	char c;
 
-	options->dataType = _DNA_DATA;
+	options->dataType = -1;
 	options->randomizations = 100;
 	options->minCompatibility = 0;
 	options->minPOC = 0.0;
 	options->maxMNIC = INT_MAX;
 	options->maxEntropy = DBL_MAX;
+	options->help = 0;
 
 	while ( (c = getopt(argc, argv, "adi:r:s:o:c:p:m:e:v")) != -1)
 	{
 		switch (c)
 		{
+			case 'i':
+				options->inputAlignment = optarg;
+				break;
 			case 'a':
 				options->dataType = _AA_DATA;
 				break;
 			case 'd':
 				options->dataType = _DNA_DATA;
-				break;
-			case 'i':
-				options->inputAlignment = optarg;
 				break;
 			case 'r':
 				options->randomizations = atoi(optarg);
@@ -71,6 +73,9 @@ void parseArguments(int argc, char** argv, Options *options)
 			case 'v':
 				verbose = 1;
 				break;
+			case 'h':
+				options->help = 1;
+				break;
 			default:
 				cerr << "Unknown parameter: " << c << endl;
 		}
@@ -79,16 +84,41 @@ void parseArguments(int argc, char** argv, Options *options)
 
 void printSyntax()
 {
+	cout << "Syntax:" << endl;
+	cout << "  shuffle -i <FILE> <-a|-d> [-r NUM] [-o FILE [-c NUM] [-p NUM] [-m NUM] -[e NUM]] [-s FILE] [-v]" << endl;
+	cout << "  shuffle -h" << endl;
+	cout << endl;
+
+	cout << "Options:" << endl;
+	cout << "  -i\tInput alignment" << endl;
+	cout << "  -a\tInput alignment is AA data" << endl;
+	cout << "  -d\tInput alignment is DNA data" << endl;
+	cout << "  -r\tNumber of randomizations for POC computations [default: 100]" << endl;
+	cout << "  -o\tOutput alignment" << endl;
+	cout << "  -c\tMinimum compatibility score [default: 0]" << endl;
+	cout << "  -p\tMinimum POC score [default: 0.0]" << endl;
+	cout << "  -m\tMaximum MNIC [default: " << INT_MAX << "]" << endl;
+	cout << "  -e\tMaximum entropy [default: " << DBL_MAX << "]" << endl;
+	cout << "  -s\tOutput file for site summary" << endl;
+	cout << "  -v\tBe verbose " << endl;
+	cout << "  -h\tThis help page" << endl;
+	cout << endl;
+
+	cout << "Note:" << endl;
+	cout << "  .phy or .phylip extensions indicate sequential Phylip file format" << endl;
+	cout << "  .fsa or .fasta extensions indicate Fasta file format" << endl;
+
+
 	exit(254);
 }
 
 int main(int argc, char** argv) {
 	Options options;
 
-	cout << PROGNAME << " " << VERSION << " [" << PROGDATE << "]" << endl;
+	cout << PROGNAME << " " << VERSION << " [" << PROGDATE << "]" << endl << endl;
 
 	parseArguments(argc, argv, &options);
-	if (!options.inputAlignment.length())
+	if (!(options.inputAlignment.length() && options.dataType >= 0) || options.help)
 		printSyntax();
 
 	Alignment alignment(options.inputAlignment, options.dataType);
